0019fc46f4600322d92e16a5f8515b7b73215a86
Mohamed Sherif <moma652@gmail.com>
2017-09-02 20:15:15 +0200

    updated project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { NavigationComponent } from './navigation/navigation.component';
+import { NewsComponent } from './news/news.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OfferComponent } from './offer/offer.component';
+import { PackagesComponent } from './packages/packages.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationComponent,
+    NewsComponent,
+    DashboardComponent,
+    OfferComponent,
+    PackagesComponent,
+    FeedbackComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'news',
+        component: NewsComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'nav',
+        component: NavigationComponent
+      },
+      {
+        path: 'offer',
+        component: OfferComponent
+      },
+      {
+        path: 'packages',
+        component: PackagesComponent
+      },
+      {
+        path: 'feedback',
+        component: FeedbackComponent
+      },
+      {
+        path: 'users',
+        component: UsersComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
