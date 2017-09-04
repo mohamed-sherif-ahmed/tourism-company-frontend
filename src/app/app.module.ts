a4cd507b00e8e59964c61e52e0484fb0ec3a2547
Mohamed Sherif <moma652@gmail.com>
2017-09-04 22:46:09 +0200

    created initial packages services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { OfferComponent } from './offer/offer.component';
 import { PackagesComponent } from './packages/packages.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { UsersComponent } from './users/users.component';
-import { FormsModule }   from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
---
+++ src/app/packages/package.ts
@@ -0,0 +1,5 @@
+export class Package {
+  name: string;
+  id: number;
+  img: string;
+}
