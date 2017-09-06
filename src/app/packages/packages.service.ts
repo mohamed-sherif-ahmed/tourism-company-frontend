f930936895c42dcf88c5f9c9691c40bcde3ff6bb
Mohamed Sherif <moma652@gmail.com>
2017-09-06 15:18:33 +0200

    added vouchers services, html

--- src/app/packages/packages.service.ts
+++ src/app/packages/packages.service.ts
@@ -6,6 +6,7 @@ import {AppConstants} from '../app.constants';
 import {RequestMethod, RequestOptions} from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
+import {Voucher} from './voucher';
 
 @Injectable ()
 export class PackagesService {
@@ -46,6 +47,40 @@ export class PackagesService {
     });
     return this.http.request(url, options).toPromise().then(response => {
       return response.json();
+    });
+  }
+  addVouchers(data: Voucher): void {
+    const url = `${AppConstants.API_ENDPOINT}/voucher`;
+    const api_key = localStorage.getItem('api_key');
+    const user_id = localStorage.getItem('user_id');
+    this.http.post(url, {
+      'api_key': api_key,
+      'user_id': user_id,
+      'package_id': 1,
+      'voucher': data
+    });
+  }
+  getVoucher(package_id: number): Promise<JSON> {
+    const url = `${AppConstants.API_ENDPOINT}/voucher/${package_id}`;
+    const options = new RequestOptions({
+      method: RequestMethod.Get,
+      body: {
+        'package_id': package_id
+      }
+    });
+    return this.http.request(url, options).toPromise().then(response => {
+      return response.json();
+    });
+  }
+  editVoucher(package_id: number, data: Voucher): void {
+    const url = `${AppConstants.API_ENDPOINT}/voucher/edit/${package_id}`;
+    const api_key = localStorage.getItem('api_key');
+    const user_id = localStorage.getItem('user_id');
+    this.http.post(url, {
+      'package_id': package_id,
+      'api_key': api_key,
+      'user_id': user_id,
+      'voucher': data
     });
   }
 }
---
+++ src/app/packages/voucher.ts
@@ -0,0 +1,8 @@
+export class Voucher {
+  title: string;
+  description: string;
+  exp_date: string;
+  points: number;
+  condition: string;
+  package_id: number;
+}
