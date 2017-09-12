4b412671cb0b40a51735520fa0e83c841bf3cf92
hisham <m.hisham@live.com>
2017-09-12 15:54:03 +0200

    added a fine looking grid

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -2,15 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 import {OfferService} from './offer.service';
 import {Offer} from './Offer' ;
+import {Test} from "./Test";
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.css']
+  styleUrls: ['./offer.component.less']
 })
+
 export class OfferComponent implements OnInit {
   constructor(private offerService: OfferService) { }
  offersArray: Offer[];
   selectedoffer: Offer;
+   tests: Test[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
   ngOnInit() {
   }
   viewOffers(date: Date , long: number): void {
