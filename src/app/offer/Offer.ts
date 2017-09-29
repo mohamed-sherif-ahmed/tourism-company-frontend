f7ca33241a4ffb66a267fe2f5c2569b81c09b16b
hisham <m.hisham@live.com>
2017-09-29 17:56:08 +0200

    offersEdited

--- src/app/offer/Offer.ts
+++ src/app/offer/Offer.ts
@@ -1,10 +1,31 @@
 export class Offer {
-title: string;
-desc: string;
-expdate: Date;
-givenpoints: number ;
-img: string ;
+name: string;
+description: string;
+exp_date: string;
+given_points: number ;
+img_path: string ;
 type: string ;
+condition_type: string;
 condition: string;
-points: number;
+constructor (
+              name:         string,
+              description:    string,
+              exp_date:    string,
+              given_points:    number,
+              img_path:    string,
+              type:        string,
+              condition_type:     string,
+              condition:       string,
+             )  {
+                this.name       = name
+                this.description   = description
+                this.exp_date  = exp_date
+                this.given_points  = given_points
+                this.img_path      = img_path
+                this.type  = type
+                this.condition_type      = condition_type
+                this.condition   = condition
+
+              }
+
 }
